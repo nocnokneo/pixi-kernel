a70097cb6989ac751e2c4467680c01e022d7f3fb
Taylor Braun-Jones <taylor@braun-jones.org>
2025-06-18 01:22:34 -0400

    Add support for a configurable fallback environment
    Using the PIXI_KERNEL_FALLBACK_ENVIRONMENT environment variable, users can now
    specify a fallback pixi environment to use if one cannot be determined from
    the Notebook metadata.

--- pixi_kernel/provisioner.py
+++ pixi_kernel/provisioner.py
@@ -44,30 +44,27 @@ class PixiKernelProvisioner(LocalProvisioner):  # type: ignore[misc]
 
         env: dict[str, str] = kwargs.get("env", os.environ.copy())
 
-        # https://github.com/jupyterlab/jupyterlab/issues/16282
-        notebook_path = env.get("JPY_SESSION_NAME")
-        if notebook_path is None:
-            self.log.error(
-                "Failed to get notebook path from JPY_SESSION_NAME variable."
-                "Falling back to the default environment."
-            )
-            environment_name = "default"
-        else:
-            try:
-                notebook = json.loads(Path(notebook_path).read_text())
-                environment_name = notebook["metadata"]["pixi-kernel"]["environment"]
-            except Exception:
-                self.log.exception(
-                    "Failed to get Pixi environment name from notebook metadata."
-                    "Falling back to default environment."
-                )
-                environment_name = "default"
-
         # If a new notebook is saved with the Pixi-kernel environment selection panel opened, the
         # environment field in the Notebook metadata could become an empty string.
         # https://github.com/renan-r-santos/pixi-kernel/issues/43#issuecomment-2676320749
+        environment_name = ""
+
+        # https://github.com/jupyterlab/jupyterlab/issues/16282
+        notebook_path = env.get("JPY_SESSION_NAME")
+        if notebook_path is None:
+            self.log.error("Failed to get notebook path from JPY_SESSION_NAME variable.")
+        else:
+            try:
+                # Set encoding to utf-8 to avoid issues on Windows
+                # https://github.com/renan-r-santos/pixi-kernel/issues/55
+                notebook = json.loads(Path(notebook_path).read_text(encoding="utf-8"))
+                environment_name = notebook["metadata"]["pixi-kernel"]["environment"]
+            except Exception:
+                self.log.exception("Failed to get Pixi environment name from notebook metadata.")
+
         if environment_name == "":
-            environment_name = "default"
+            environment_name = os.environ.get("PIXI_KERNEL_FALLBACK_ENVIRONMENT", "default")
+            self.log.info(f"Falling back to the '{environment_name}' Pixi environment.")
 
         result = await verify_env_readiness(
             environment_name=environment_name,
